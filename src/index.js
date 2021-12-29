e140be49707352259e4b0390bf08f1bf4a70eded
Cristina <cristoshiwassaki@gmail.com>
2021-12-28 21:43:44 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-
+import './index.css';
 
 ReactDOM.render(
-  <App />, document.getElementById('root'));
+  <React.StrictMode>
+    <App /> 
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 
